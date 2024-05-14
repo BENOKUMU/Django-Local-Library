cd5e4d8e4fe95fd39308286c46a3219f96dae8a8
Benjamin Okumu <benjaminokumu12@gmail.com>
2024-05-14 21:35:36 +0300

    Created models, BookInstance/ Author

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import lower
+import uuid
 
 # Create your models here.
 # Genre Model
@@ -49,5 +50,51 @@ class Book(models.Model):
     
     def get_absolute_url(self):
         return reverse("book-detail", args=[str(self.id)])
+    
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+    
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+    
+    class Meta:
+        ordering = ['due_back']
+        
+    def __str__(self):
+        """String for representing the Model object."""
+        return  f'{self.id} ({self.book.title})'
+    
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)    
+    
+    class Meta:
+        ordering = ['last_name', 'first_name']
+        
+    def get_absolute_url(self):
+        return reverse("author-detail", args=[str(self.id)])
+    
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
     
     
