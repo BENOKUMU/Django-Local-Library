2f8bc19357ce6ede2afa3a01a9ee49a2be56ff26
Benjamin Okumu <benjaminokumu12@gmail.com>
2024-05-14 21:08:09 +0300

    models created, Book, Genre

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.urls import reverse
+from django.db.models import UniqueConstraint
+from django.db.models.functions import lower
 
 # Create your models here.
+# Genre Model
+class Genre(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+    )
+    def __str__(self):
+        """String for represeting a book genre."""
+        return self.name
+    
+    def get_absolute_url(self):
+        """Returns the url to access a particular genre instance."""
+        return reverse('genre-detail', args=[str(self.id)])
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='genre_name_case_insensitive_unique',
+                violation_error_message = "Genre already exists (case insensitive match)"
+            )
+        ]
+
+class Book(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of the book"
+    )
+    isbn = models.CharField('ISBN', max_length=13,
+                            unique=True,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
+    genre = models.ManyToManyField(
+        Genre, help_text="Select a genre for this book"
+    )
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[str(self.id)])
+    
+    
