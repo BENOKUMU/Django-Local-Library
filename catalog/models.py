9a77d2064f936cac3c67080e52d493191703d7b4
Benjamin Okumu <benjaminokumu12@gmail.com>
2024-05-15 14:02:34 +0300

    updated and modified, admin, model

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.db.models import UniqueConstraint
-from django.db.models.functions import lower
+from django.db.models.functions import Lower
 import uuid
 
 # Create your models here.
@@ -30,6 +30,31 @@ class Genre(models.Model):
             )
         ]
 
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            unique=True,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+    
+    def get_absolute_url(self):
+        """Returns the url to access a particular language instance."""
+        return reverse("language-detail", args=[str(self.id)])
+    
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name = 'language_name_case_insensitive_unique',
+                violation_error_message = "Language already exists (case insensitive match)"
+            ),
+        ]
+    
+     
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
@@ -44,12 +69,24 @@ class Book(models.Model):
     genre = models.ManyToManyField(
         Genre, help_text="Select a genre for this book"
     )
+    language = models.ForeignKey(
+        'Language', on_delete=models.SET_NULL, null=True
+    )
+    
+    class Meta:
+        ordering = ['title', 'author']
     
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse("book-detail", args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre'
+    
     
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
