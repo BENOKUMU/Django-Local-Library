5d65df36884c4b5885a0e21077158702655ebf53
Benjamin Okumu <benjaminokumu12@gmail.com>
2024-05-20 14:51:39 +0300

    User authentication and permissions

--- DjangoLocalLibrary/settings.py
+++ DjangoLocalLibrary/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'DjangoLocalLibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
